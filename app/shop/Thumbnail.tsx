e27ee2b1f4926d345f67bdeed3f55637cb2aab85
johan piquet <joh.pik.fr@gmail.com>
2024-02-19 12:06:07 +1000

    feat: add images to thumbnail prods

--- app/shop/Thumbnail.tsx
+++ app/shop/Thumbnail.tsx
@@ -18,13 +18,19 @@ const Thumbnail = ({
   image,
   inCart = false,
 }: ThumbnailProps) => {
-  console.log(image.url);
   return (
     <div key={id} className="product-container">
       {inCart ? <div className="already-in-cart">Already in Cart</div> : null}
       <Link href={url}>
         <div className="img-container">
-          <Image src={image.url} alt={name} className="image-thumbnail" />
+          <Image
+            src={image.url}
+            alt={name}
+            className="image-thumbnail"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1160px) 33vw, 25vw"
+            priority
+          />
         </div>
       </Link>
 
